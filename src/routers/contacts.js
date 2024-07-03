46b7691a274d9ec5e9e4d168736630e38f34353c
Volodimir Kovalenko <volodimirkovalenko@Volodimirs-MacBook-Pro.local>
2024-07-03 22:46:53 +0300

    add files

--- src/routers/contacts.js
+++ src/routers/contacts.js
@@ -2,6 +2,10 @@ import { Router } from 'express';
 import {
   getContactsControllers,
   getContactByIdControllers,
+  addContactControllers,
+  updateContactControllers,
+  patcContactController,
+  deleteContactController,
 } from '../conrollers/contacts.js';
 import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 import isValidId from '../middlewares/isValidId.js';
@@ -14,6 +18,26 @@ ContactsRouter.get(
   '/:contactId',
   isValidId,
   ctrlWrapper(getContactByIdControllers),
+);
+
+ContactsRouter.post('/', ctrlWrapper(addContactControllers));
+
+ContactsRouter.put(
+  '/:contactId',
+  isValidId,
+  ctrlWrapper(updateContactControllers),
+);
+
+ContactsRouter.patch(
+  '/:contactId',
+  isValidId,
+  ctrlWrapper(patcContactController),
+);
+
+ContactsRouter.delete(
+  '/:contactId',
+  isValidId,
+  ctrlWrapper(deleteContactController),
 );
 
 export default ContactsRouter;
